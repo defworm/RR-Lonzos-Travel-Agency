a73ae1e9fe9168b30d995592ee5fcf246aa630b5
Crystal Korth <crystalkorth@gmail.com>
2022-08-28 15:54:30 -0500

    worked on suspense

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import loading_spinner from '../palm_anim_v2.gif'
+
+const Spinner = () => {
+    return (
+        <div>
+            <img src={loading_spinner} ald='loading...' />
+        </div>
+    )
+}
+
+export default Spinner 
